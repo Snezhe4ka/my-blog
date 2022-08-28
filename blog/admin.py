3724012878648756e29760b36d80106b1bbc13d1
Snezhe4ka <dovha2802@gmail.com>
2022-08-28 16:57:40 +0200

    Add comments, fix bugs, refactoring

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Post
+
+from .models import Post, Comment
 
 admin.site.register(Post)
-
-# Register your models here.
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,12 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def not_approved_comments(self):
+        return self.comments.filter(approved_comment=False)
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
@@ -17,3 +23,17 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
